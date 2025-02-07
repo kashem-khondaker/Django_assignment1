ba63155d29f24192e606e90d0680303e0f5f6006
Kashem Khondaker <kashem.khondaker.official001@gmail.com>
2025-02-08 00:02:37 +0600

    use dynamic url in home dashboard , event , participants

--- events/urls.py
+++ events/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from events.views import Filter,  add_events,Search,Organizer_Dashboard , update_events , delete_events , Details
+from events.views import Filter,Home,  add_events,Search,Organizer_Dashboard , update_events , delete_events , Details
 
 urlpatterns = [
     # path('admin/' , admin.site.urls),
     path('create-event' , add_events , name="add_event"),
     path('update-event/<int:id>/' , update_events , name="update_event"),
     path('delete-event/<int:id>/' , delete_events , name="delete_events"),
-    # path('home/',Home,name="home"),
+    path('home/',Home,name="home"),
     path('organizer-dashboard/',Organizer_Dashboard,name="Organizer_Dashboard"),
     path('search/',Search,name="Search"),
     path('filter/',Filter , name="filter"),
